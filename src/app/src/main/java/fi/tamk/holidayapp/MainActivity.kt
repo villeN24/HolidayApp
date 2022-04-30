951c31c0d564634f8b6ba92ba0a539c57018f96b
Ville Naalisvaara <ville.naalisvaara@tuni.fi>
2022-04-30 12:20:47 +0300

    Create a base spinner list.

--- src/app/src/main/java/fi/tamk/holidayapp/MainActivity.kt
+++ src/app/src/main/java/fi/tamk/holidayapp/MainActivity.kt
@@ -1,11 +1,30 @@
 package fi.tamk.holidayapp
 
+import android.content.Context
+import android.net.wifi.WifiManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.Spinner
+
+val allCountries = "https://calendarific.com/api/v2/countries?api_key=82412897a0bd6afebfd64c44eab3013ba5c88a52"
 
 class MainActivity : AppCompatActivity() {
+    lateinit var spinner : Spinner
+    lateinit var adapter : ArrayAdapter<String>
+    var testList = mutableListOf("test1", "test2", "test3")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.spinner = findViewById(R.id.spinnerList)
+        adapter = ArrayAdapter<String>(
+            this,
+            R.layout.list_item,
+            testList)
+        this.spinner.adapter = adapter
+
     }
 }
