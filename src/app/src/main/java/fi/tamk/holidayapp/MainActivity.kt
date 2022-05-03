848883f03e1f665e2d7cbab3b772d2fc7f825a7e
Ville Naalisvaara <ville.naalisvaara@tuni.fi>
2022-05-03 16:12:08 +0300

    Add filtering to the spinner element.

--- src/app/src/main/java/fi/tamk/holidayapp/MainActivity.kt
+++ src/app/src/main/java/fi/tamk/holidayapp/MainActivity.kt
@@ -4,17 +4,20 @@ import android.content.Context
 import android.net.wifi.WifiManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.ListView
 import android.widget.Spinner
+import androidx.core.widget.addTextChangedListener
 
 val allCountries = "https://calendarific.com/api/v2/countries?api_key=82412897a0bd6afebfd64c44eab3013ba5c88a52"
 
 class MainActivity : AppCompatActivity() {
     lateinit var spinner : Spinner
     lateinit var adapter : ArrayAdapter<String>
-    var testList = mutableListOf("test1", "test2", "test3")
+    lateinit var filterList : EditText
+    var testList = mutableListOf("matti", "maali", "timppa", "pauli")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +28,15 @@ class MainActivity : AppCompatActivity() {
             R.layout.list_item,
             testList)
         this.spinner.adapter = adapter
-
+        this.filterList = findViewById(R.id.filterList)
+        this.filterList.addTextChangedListener { if(filterList.text.toString() != "") updateList() }
+    }
+    private fun updateList() {
+        val updatedList = testList.filter { it.contains(filterList.text.toString()) }
+        adapter = ArrayAdapter<String>(
+            this,
+            R.layout.list_item,
+            updatedList)
+        this.spinner.adapter = adapter
     }
 }
